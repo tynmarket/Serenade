7abfdfe1baea910532e48a91a2abf2a4cb2cb8ba
Takehiko Shinkura <tynmarket@gmail.com>
2017-12-31 16:24:02 +0900

    Twitter OAuth

--- app/src/main/java/com/tynmarket/serenade/activity/LoginActivity.java
+++ app/src/main/java/com/tynmarket/serenade/activity/LoginActivity.java
@@ -1,9 +1,16 @@
 package com.tynmarket.serenade.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 import com.tynmarket.serenade.R;
 
 /**
@@ -11,9 +18,34 @@ import com.tynmarket.serenade.R;
  */
 
 public class LoginActivity extends AppCompatActivity {
+    private TwitterLoginButton mLoginButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setTwitterLoginButtonCallback();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setTwitterLoginButtonCallback() {
+        mLoginButton = findViewById(R.id.login_button);
+        mLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Toast.makeText(LoginActivity.this, "ログイン成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(LoginActivity.this, "ログイン失敗", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
