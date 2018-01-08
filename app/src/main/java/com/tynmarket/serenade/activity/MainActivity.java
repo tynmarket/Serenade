c51d32187c0dd759c876f820a036d27bc25e493a
Takehiko Shinkura <tynmarket@gmail.com>
2018-01-08 17:15:28 +0900

    Hold Fragment state

--- app/src/main/java/com/tynmarket/serenade/activity/MainActivity.java
+++ app/src/main/java/com/tynmarket/serenade/activity/MainActivity.java
@@ -76,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOffscreenPageLimit(2);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
