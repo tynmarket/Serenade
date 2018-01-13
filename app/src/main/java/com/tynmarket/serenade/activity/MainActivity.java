d7dc0224cf6adba39fcc5912fe47cac0eaeabd7b
Takehiko Shinkura <tynmarket@gmail.com>
2018-01-14 00:21:41 +0900

    Load hometimeline by touching FAB

--- app/src/main/java/com/tynmarket/serenade/activity/MainActivity.java
+++ app/src/main/java/com/tynmarket/serenade/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.tynmarket.serenade.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -85,12 +84,8 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        fab.setOnClickListener((View view) -> {
+            loadHomeTimeline();
         });
 
         initTwitterConfig();
