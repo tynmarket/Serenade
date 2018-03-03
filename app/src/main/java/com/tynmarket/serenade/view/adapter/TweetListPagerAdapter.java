53b11d3b3d2187b59d1affdb3cba2cfdeec66980
Takehiko Shinkura <tynmarket@gmail.com>
2018-03-03 11:27:47 +0900

    Remove third view

--- app/src/main/java/com/tynmarket/serenade/view/adapter/TweetListPagerAdapter.java
+++ app/src/main/java/com/tynmarket/serenade/view/adapter/TweetListPagerAdapter.java
@@ -24,8 +24,7 @@ public class TweetListPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 3 total pages.
-        return 3;
+        return 2;
     }
 
     @Override
