abdf05e88433f0125465d41c3a54e8b73de8ddc4
Takehiko Shinkura <tynmarket@gmail.com>
2018-03-17 15:47:42 +0900

    Replace findViewById with DataBinding on TweetContentView

--- app/src/main/java/com/tynmarket/serenade/view/custom/TweetContentView.java
+++ app/src/main/java/com/tynmarket/serenade/view/custom/TweetContentView.java
@@ -2,13 +2,15 @@ package com.tynmarket.serenade.view.custom;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.twitter.sdk.android.core.models.Tweet;
 import com.tynmarket.serenade.R;
+import com.tynmarket.serenade.databinding.TweetContentBinding;
 import com.tynmarket.serenade.model.util.TwitterUtil;
 
 /**
@@ -17,7 +19,8 @@ import com.tynmarket.serenade.model.util.TwitterUtil;
 
 public class TweetContentView extends RelativeLayout {
     public Tweet tweet;
-    private TextView tweetText;
+
+    private TweetContentBinding binding;
 
     public TweetContentView(Context context) {
         super(context);
@@ -26,8 +29,8 @@ public class TweetContentView extends RelativeLayout {
     public TweetContentView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        inflate(context, R.layout.tweet_content, this);
-        this.tweetText = findViewById(R.id.tweet_text);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        binding = DataBindingUtil.inflate(inflater, R.layout.tweet_content, this, true);
 
         // Open tweet
         setOnTweetTextClickListener();
@@ -38,7 +41,7 @@ public class TweetContentView extends RelativeLayout {
     }
 
     private void setOnTweetTextClickListener() {
-        tweetText.setOnClickListener(v -> {
+        binding.tweetText.setOnClickListener(v -> {
             Uri uri = TwitterUtil.tweetUri(tweet.user.screenName, tweet.idStr);
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
             // TODO: FLAG_ACTIVITY
