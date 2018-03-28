79a301307ef249f4536bc5a6595fdd5bfb0fc280
Takehiko Shinkura <tynmarket@gmail.com>
2018-03-28 21:55:40 +0900

    Fix a bug that the favorite count does not increment when favorited a retweet

--- app/src/main/java/com/tynmarket/serenade/model/TweetMapper.java
+++ app/src/main/java/com/tynmarket/serenade/model/TweetMapper.java
@@ -1,5 +1,7 @@
 package com.tynmarket.serenade.model;
 
+import android.support.annotation.Nullable;
+
 import com.twitter.sdk.android.core.models.Tweet;
 
 /**
@@ -10,11 +12,17 @@ public class TweetMapper {
     public static Tweet withFavorited(Tweet tweet, boolean favorited) {
         int favoriteCount = tweet.favoriteCount;
 
+        if (tweet.retweetedStatus != null) {
+            favoriteCount = tweet.retweetedStatus.favoriteCount;
+        }
+
         if (favorited) {
             favoriteCount += 1;
         } else if (favoriteCount > 0) {
             favoriteCount -= 1;
         }
+
+        Tweet retweetedStatus = withFavoriteCount(tweet.retweetedStatus, favoriteCount);
 
         return new Tweet(
                 tweet.coordinates,
@@ -41,7 +49,7 @@ public class TweetMapper {
                 tweet.quotedStatus,
                 tweet.retweetCount,
                 tweet.retweeted,
-                tweet.retweetedStatus,
+                retweetedStatus,
                 tweet.source,
                 tweet.text,
                 tweet.displayTextRange,
@@ -89,6 +97,50 @@ public class TweetMapper {
                 tweet.quotedStatus,
                 retweetCount,
                 retweeted,
+                tweet.retweetedStatus,
+                tweet.source,
+                tweet.text,
+                tweet.displayTextRange,
+                tweet.truncated,
+                tweet.user,
+                tweet.withheldCopyright,
+                tweet.withheldInCountries,
+                tweet.withheldScope,
+                tweet.card
+        );
+    }
+
+    @Nullable
+    private static Tweet withFavoriteCount(@Nullable Tweet tweet, int favoriteCount) {
+        if (tweet == null) {
+            return null;
+        }
+
+        return new Tweet(
+                tweet.coordinates,
+                tweet.createdAt,
+                tweet.currentUserRetweet,
+                tweet.entities,
+                tweet.extendedEntities,
+                favoriteCount,
+                tweet.favorited,
+                tweet.filterLevel,
+                tweet.id,
+                tweet.idStr,
+                tweet.inReplyToScreenName,
+                tweet.inReplyToStatusId,
+                tweet.inReplyToStatusIdStr,
+                tweet.inReplyToUserId,
+                tweet.inReplyToUserIdStr,
+                tweet.lang,
+                tweet.place,
+                tweet.possiblySensitive,
+                tweet.scopes,
+                tweet.quotedStatusId,
+                tweet.quotedStatusIdStr,
+                tweet.quotedStatus,
+                tweet.retweetCount,
+                tweet.retweeted,
                 tweet.retweetedStatus,
                 tweet.source,
                 tweet.text,
