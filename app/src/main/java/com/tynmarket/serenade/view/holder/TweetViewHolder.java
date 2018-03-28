59be1cb26457d595588a3886ac996ef20daf8cd7
Takehiko Shinkura <tynmarket@gmail.com>
2018-03-28 22:33:05 +0900

    Show the user page when click a retweeted user profile

--- app/src/main/java/com/tynmarket/serenade/view/holder/TweetViewHolder.java
+++ app/src/main/java/com/tynmarket/serenade/view/holder/TweetViewHolder.java
@@ -107,7 +107,8 @@ public class TweetViewHolder extends RecyclerView.ViewHolder {
         // http://snowrobin.tumblr.com/post/62229276876/androidimageview%E3%81%AB%E3%82%A8%E3%83%95%E3%82%A7%E3%82%AF%E3%83%88%E3%82%92%E4%BB%98%E4%B8%8E%E3%81%99%E3%82%8B
         binding.profile.setOnClickListener(v -> {
             // TODO: Open correct profile when RT/QT
-            Uri uri = TwitterUtil.profileUri(getTweet().user.screenName);
+            User user = TweetUtil.tweetOrRetweetedStatus(getTweet()).user;
+            Uri uri = TwitterUtil.profileUri(user.screenName);
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
             // TODO: Transition
             // https://developer.android.com/reference/android/app/Activity.html#overridePendingTransition(int, int)
