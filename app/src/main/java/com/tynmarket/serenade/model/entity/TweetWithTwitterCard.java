7b383acf9ec96947dd164bb406b9636e4d9bd0e2
Takehiko Shinkura <tynmarket@gmail.com>
2018-04-28 21:39:52 +0900

    Fix bug that the twitter card url is revealed when favorited or retweeted

--- app/src/main/java/com/tynmarket/serenade/model/entity/TweetWithTwitterCard.java
+++ app/src/main/java/com/tynmarket/serenade/model/entity/TweetWithTwitterCard.java
@@ -1,6 +1,13 @@
 package com.tynmarket.serenade.model.entity;
 
+import com.twitter.sdk.android.core.models.Card;
+import com.twitter.sdk.android.core.models.Coordinates;
+import com.twitter.sdk.android.core.models.Place;
 import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.models.TweetEntities;
+import com.twitter.sdk.android.core.models.User;
+
+import java.util.List;
 
 /**
  * Created by tynmarket on 2018/04/28.
@@ -45,6 +52,30 @@ public class TweetWithTwitterCard extends Tweet {
                 tweet.withheldScope,
                 tweet.card
         );
+        this.twitterCard = twitterCard;
+    }
+
+    public TweetWithTwitterCard(Coordinates coordinates, String createdAt, Object currentUserRetweet,
+                                TweetEntities entities, TweetEntities extendedEntities, Integer favoriteCount,
+                                boolean favorited, String filterLevel, long id, String idStr,
+                                String inReplyToScreenName, long inReplyToStatusId, String inReplyToStatusIdStr,
+                                long inReplyToUserId, String inReplyToUserIdStr, String lang, Place place,
+                                boolean possiblySensitive, Object scopes, long quotedStatusId, String quotedStatusIdStr,
+                                Tweet quotedStatus, int retweetCount, boolean retweeted, Tweet retweetedStatus,
+                                String source, String text, List<Integer> displayTextRange, boolean truncated,
+                                User user, boolean withheldCopyright, List<String> withheldInCountries,
+                                String withheldScope, Card card, TwitterCard twitterCard) {
+
+        super(coordinates, createdAt, currentUserRetweet,
+                entities, extendedEntities, favoriteCount,
+                favorited, filterLevel, id, idStr,
+                inReplyToScreenName, inReplyToStatusId, inReplyToStatusIdStr,
+                inReplyToUserId, inReplyToUserIdStr, lang, place,
+                possiblySensitive, scopes, quotedStatusId, quotedStatusIdStr,
+                quotedStatus, retweetCount, retweeted, retweetedStatus,
+                source, text, displayTextRange, truncated,
+                user, withheldCopyright, withheldInCountries,
+                withheldScope, card);
         this.twitterCard = twitterCard;
     }
 }
