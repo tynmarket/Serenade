1b3b7b3cb114f5c089317f8dd3f7631a55d72570
Takehiko Shinkura <tynmarket@gmail.com>
2018-05-11 11:25:59 +0900

    Suppress unintended continuous refreshing

--- app/src/main/java/com/tynmarket/serenade/view/listner/InfiniteTimelineScrollListener.java
+++ app/src/main/java/com/tynmarket/serenade/view/listner/InfiniteTimelineScrollListener.java
@@ -7,15 +7,22 @@ import com.twitter.sdk.android.core.models.Tweet;
 import com.tynmarket.serenade.model.TweetList;
 import com.tynmarket.serenade.model.TwitterCardList;
 import com.tynmarket.serenade.model.util.FirebaseAnalyticsHelper;
+import com.tynmarket.serenade.model.util.LogUtil;
 import com.tynmarket.serenade.view.adapter.TweetListAdapter;
 import com.tynmarket.serenade.view.holder.TweetViewHolder;
+
+import org.threeten.bp.Duration;
+import org.threeten.bp.LocalDateTime;
 
 /**
  * Created by tynmarket on 2018/01/14.
  */
 
 public class InfiniteTimelineScrollListener extends RecyclerView.OnScrollListener {
+    private static final int REFRESH_INTERVAL = 3;
+
     public boolean mRefreshing = false;
+    private LocalDateTime mLatestRefreshing;
 
     private final int sectionNumber;
     private FirebaseAnalyticsHelper analytics;
@@ -37,8 +44,21 @@ public class InfiniteTimelineScrollListener extends RecyclerView.OnScrollListene
 
         // TODO: Not load if last tweet is loaded
         if (!mRefreshing && position + childCount == totalCount) {
+            LocalDateTime now = LocalDateTime.now();
+
+            // Suppress unintended continuous refreshing
+            if (mLatestRefreshing != null) {
+                long diff = Duration.between(mLatestRefreshing, now).getSeconds();
+
+                if (diff < REFRESH_INTERVAL) {
+                    LogUtil.d("Suppress unintended continuous refreshing");
+                    return;
+                }
+            }
+
             analytics.logReadMoreTweetList(sectionNumber);
             mRefreshing = true;
+            mLatestRefreshing = now;
 
             TweetViewHolder lastItem = (TweetViewHolder) recyclerView.findViewHolderForAdapterPosition(totalCount - 1);
             long maxId = lastItem.getTweet().id - 1;
