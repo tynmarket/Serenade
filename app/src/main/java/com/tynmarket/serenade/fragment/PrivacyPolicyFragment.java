6cfe8c41a3419c564550565792c839061705fef9
Takehiko Shinkura <tynmarket@gmail.com>
2019-06-17 19:46:38 +0900

    キャンセル復活

--- app/src/main/java/com/tynmarket/serenade/fragment/PrivacyPolicyFragment.java
+++ app/src/main/java/com/tynmarket/serenade/fragment/PrivacyPolicyFragment.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.widget.Toast;
 
 import com.tynmarket.serenade.R;
 
@@ -22,12 +23,18 @@ public class PrivacyPolicyFragment extends AppCompatDialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.privacy_title)
                 .setMessage(R.string.privacy_message)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                .setPositiveButton(R.string.text_ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getContext(), R.string.text_ok, Toast.LENGTH_SHORT).show();
                         SharedPreferences pref = getContext().getSharedPreferences(PrivacyPolicyFragment.PREF_NAME, MODE_PRIVATE);
                         SharedPreferences.Editor editor = pref.edit();
                         editor.putBoolean(PrivacyPolicyFragment.AGREE, true);
                         editor.apply();
+                    }
+                })
+                .setNegativeButton(R.string.text_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getContext(), R.string.text_cancel, Toast.LENGTH_SHORT).show();
                     }
                 });
         // Create the AlertDialog object and return it
