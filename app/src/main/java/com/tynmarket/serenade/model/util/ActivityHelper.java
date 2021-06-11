88bd23ff14411738fa6a1c91c824a0a6c0683eb5
Takehiko Shinkura <tynmarket@gmail.com>
2021-06-11 19:48:18 +0900

    OSのバージョンで出し分け

--- app/src/main/java/com/tynmarket/serenade/model/util/ActivityHelper.java
+++ app/src/main/java/com/tynmarket/serenade/model/util/ActivityHelper.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 
 import com.tynmarket.serenade.R;
 
@@ -16,8 +17,15 @@ public class ActivityHelper {
 
     public static void startUriActivity(Context context, Uri uri) {
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
         // TODO: Set only if url is twitter
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // Android 11以上の場合
+        if (Build.VERSION.SDK_INT >= 30) {
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        } else {
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        }
+
         startActivity(context, intent);
     }
 
